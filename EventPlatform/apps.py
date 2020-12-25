db88cb3f6b2f11cde2fb80513818d6bcc269f3fe
Murad011 <muradaydin122@gmail.com>
2020-12-25 23:08:17 +0400

    deploying

--- EventPlatform/apps.py
+++ EventPlatform/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class EventplatformConfig(AppConfig):
-    name = 'EventPlatform'
+    name = 'eventplatform'
--- eventback/settings.py
+++ eventback/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
-    'EventPlatform',
+    'eventplatform',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
