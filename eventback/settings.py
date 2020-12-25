f33f68fc10567f079d066a07fcaba04c04e7d2b3
Murad011 <muradaydin122@gmail.com>
2020-12-26 00:15:27 +0400

    deploying

--- eventback/settings.py
+++ eventback/settings.py
@@ -32,8 +32,8 @@ ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
     'eventplatform',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
