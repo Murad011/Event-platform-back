01d664da925d028f2ad39106f8fcd62ed73aa199
Murad011 <muradaydin122@gmail.com>
2020-12-27 15:10:56 +0400

    Login register log out serializer created

--- EventPlatform/models.py
+++ EventPlatform/models.py
@@ -38,8 +38,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_verified = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
