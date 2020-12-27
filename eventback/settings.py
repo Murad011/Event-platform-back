541e3abf21f0b5d1f8e777ae85e66ed483555fbd
Murad011 <muradaydin122@gmail.com>
2020-12-27 13:13:01 +0400

    AWS sotages changed

--- eventback/settings.py
+++ eventback/settings.py
@@ -153,3 +153,4 @@ AWS_STORAGE_BUCKET_NAME = 'event-platform-murad'
 AWS_S3_FILE_EVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
